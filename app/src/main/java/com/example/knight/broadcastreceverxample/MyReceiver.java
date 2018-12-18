64f599a3fb54456412b5ea00a398ee90da4184c8
knight <tocaldera6734@gmail.com>
2018-12-18 07:19:34 +0530

    Added broadcast receiver

---
+++ app/src/main/java/com/example/knight/broadcastreceverxample/MyReceiver.java
@@ -0,0 +1,14 @@
+package com.example.knight.broadcastreceverxample;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context,"battry low",Toast.LENGTH_SHORT).show(); 
+    }
+}
